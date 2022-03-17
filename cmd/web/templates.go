1ba347e08f700416ee60f1fe0debbb563d46c41e
XpycT <XpycT.TOP@gmail.com>
2022-03-17 14:21:01 +0200

    Form pkg

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"github.com/xpyct/snippetbox/pkg/forms"
 	"github.com/xpyct/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
